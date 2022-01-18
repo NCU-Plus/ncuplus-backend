861635f9e490d2573d848bf677feb27109294543
ppodds <oscar20020629@gmail.com>
2022-01-18 18:23:04 +0800

    Feat: add .eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', '.github/*'],
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
