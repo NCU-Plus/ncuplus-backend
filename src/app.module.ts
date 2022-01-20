c7f5b0342281c3f2e86b647de4b014d82f33d7a4
ppodds <oscar20020629@gmail.com>
2022-01-21 01:40:58 +0800

    Feat: course module template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [],
+  imports: [CourseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/course/course.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { CourseService } from './course.service';
+
+@Controller('courses')
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Get()
+  getCourses() {
+    return this.courseService.getCourses();
+  }
+}
