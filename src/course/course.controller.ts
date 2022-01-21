930dd3f0ec2111d71501f681a8971edafc7acd53
ppodds <oscar20020629@gmail.com>
2022-01-21 21:41:50 +0800

    Fix: courses CORS

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { CourseService } from './course.service';
 
 @Controller('courses')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
   @Get()
+  @Header('Access-Control-Allow-Origin', '*')
   async getCourses() {
     return {
       statusCode: 200,
