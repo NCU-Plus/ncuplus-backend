20019a08e786a72ef48c963ffdb05a794eaa6a8a
ppodds <oscar20020629@gmail.com>
2022-06-16 18:06:28 +0800

    Feat: add oauth and session setting

--- src/config/envs/default.ts
+++ src/config/envs/default.ts
@@ -3,4 +3,9 @@ export const config = {
     secret: process.env.JWT_SECRET,
     signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
   },
+  oauth: {
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    redirectUrl: process.env.OAUTH_REDIRECT_URL,
+  },
 };
