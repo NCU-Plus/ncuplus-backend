5e398921540c747a34178bb6b98225faaf08235b
ppodds <oscar20020629@gmail.com>
2022-06-30 21:55:08 +0800

    Fix: production config cause error

--- src/config/envs/test.ts
+++ src/config/envs/test.ts
@@ -1,22 +1,6 @@
-// export * from './development';
 export const config = {
   db: {
-    type: 'mysql',
-    synchronize: false,
+    synchronize: true,
     logging: false,
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
-    username: process.env.DB_USER || 'username',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'dbname',
-    extra: {
-      connectionLimit: 5,
-    },
-    // entities: [`${__dirname}/../entity/**/*.{js,ts}`],
-    // subscribers: [`${__dirname}/../subscriber/**/*.{js,ts}`],
-    // migrations: [`${__dirname}/../migration/**/*.{js,ts}`],
-  },
-  graphql: {
-    playground: false,
   },
 };
