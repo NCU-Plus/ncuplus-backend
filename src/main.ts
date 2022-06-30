305d0cd12dce55bc04aa331b36305f36da6d2482
ppodds <oscar20020629@gmail.com>
2022-07-01 03:26:53 +0800

    Feat: redis session in production environment

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createClient } from 'redis';
+import * as connectRedis from 'connect-redis';
 import helmet from 'helmet';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import { exit } from 'process';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
+  const sessionOptions: session.SessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV === 'production') {
+    const redisClient = createClient({
+      legacyMode: true,
+      url: process.env.REDIS_URL,
+    });
+    try {
+      await redisClient.connect();
+      const RedisStore = connectRedis(session);
+      sessionOptions.store = new RedisStore({ client: redisClient });
+    } catch (e) {
+      Logger.error('Unable to connect to Redis');
+      exit(1);
+    }
+  }
+  app.use(session(sessionOptions));
   app.use(passport.initialize());
   app.use(passport.session());
   await app.listen(3000);
